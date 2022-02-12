9112b95894b5ef90b62e6dc21f4f29f3b0a47567
ricardoyepezz <ricardoyepezz@gmail.com>
2022-02-12 20:35:31 +0000

    changes html, add code to app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = ["com", "net", "us", "io"];
+
+  for (let j = 0; j < adj.length; j++) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + "." + ext[l];
+          console.log(domain);
+
+          document.getElementById("domain").innerHTML = domain;
+        }
+      }
+    }
+  }
 };
